2358ef8625ed0c4ca0422efb320a197fa738adb7
Cesar <Veliz>
2019-09-13 01:12:31 -0500

    se agrego firebase al login solo entrando con cuenta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { NologinGuard } from './guards/nologin.guard';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule', canActivate : [NologinGuard] },
   { path: 'registrar', loadChildren: './registrar/registrar.module#RegistrarPageModule' },
   { path: 'recuperar', loadChildren: './recuperar/recuperar.module#RecuperarPageModule' },
   { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'ropa', loadChildren: './ropa/ropa.module#RopaPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate : [AuthGuard] },
+  { path: 'ropa', loadChildren: './ropa/ropa.module#RopaPageModule' },
   { path: 'electronica', loadChildren: './electronica/electronica.module#ElectronicaPageModule' },
   { path: 'servicios', loadChildren: './servicios/servicios.module#ServiciosPageModule' },
 
